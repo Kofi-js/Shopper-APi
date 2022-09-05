005d3cf6cd5113d7e3f6a7d43ac83626f15fbb7f
iknow6aint <saintagbukor@gmail.com>
2022-09-04 21:19:06 -0300

    mongo connect

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const app = require('./src/app');
+const { mongoConnect } = require('./src/services/mongo');
 
 const PORT = 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`listenig on ${PORT}`);
-});
+async function startServer() {
+  await mongoConnect();
+
+  server.listen(PORT, () => {
+    console.log(`listenig on ${PORT}`);
+  });
+}
+startServer();
