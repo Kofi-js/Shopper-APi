4804ec92518f46ef5264d7c0a837e66fd6051fac
iknow6aint <saintagbukor@gmail.com>
2022-09-07 12:01:19 -0300

    update product router

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { createProduct } = require('../controllers/productController');
+const { createProduct, updateProduct } = require('../controllers/productController');
 const { isAdmin, isVendor, isAuth } = require('../utils/verifyToken');
 // express init
 const ProductRouter = express.Router();
 
 // routes
 ProductRouter.post('/', isVendor, createProduct);
-
+ProductRouter.put('/:id', isVendor, updateProduct);
 module.exports = ProductRouter;
