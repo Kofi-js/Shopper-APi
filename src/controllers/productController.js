522b7ec45f3c06db05a8fed071f1212969fbcbbb
iknow6aint <saintagbukor@gmail.com>
2022-09-08 18:43:01 -0300

    get allproduct

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -45,9 +45,32 @@ async function getProduct(req, res){
   }
 }
 
+// GET ALL PRODUCTS
+async function getAllProducts(req, res){
+  const qNew = req.query.new;
+  const qCategory = req.query.new
+  try {
+    let products;
+    if (qNew){
+      products = await Product.find().sort({ createdAt: -1 }).limit(5)
+    } else if (qCategory){
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find()
+    }
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+}
 module.exports = {
   createProduct,
   updateProduct,
   deleteProduct,
   getProduct,
+  getAllProducts,
 }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {
-  createProduct, updateProduct, deleteProduct, getProduct,
+  createProduct, updateProduct, deleteProduct, getProduct, getAllProducts,
 } = require('../controllers/productController');
 const { authIsVendor } = require('../utils/verifyToken');
 // express init
@@ -11,5 +11,6 @@ ProductRouter.post('/api/products/add', authIsVendor, createProduct);
 ProductRouter.put('/api/products/:id', authIsVendor, updateProduct);
 ProductRouter.delete('/api/products/:id', authIsVendor, deleteProduct);
 ProductRouter.get('/api/products/find/:id', authIsVendor, getProduct);
+ProductRouter.get('/api/products', authIsVendor, getAllProducts);
 
 module.exports = ProductRouter;
